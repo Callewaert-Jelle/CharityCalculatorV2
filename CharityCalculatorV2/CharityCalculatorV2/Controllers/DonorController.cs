d99cf90f160a2ce4862232c517030957da76d876
Jelle Callewaert <jelle.callewaert@hotmail.com>
2020-08-01 13:48:12 +0200

    Fixed calculator implemented

---
+++ CharityCalculatorV2/CharityCalculatorV2/Controllers/DonorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CharityCalculatorV2.Models.DonationViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharityCalculatorV2.Controllers
+{
+    public class DonorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(DonationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = CalculateDeductableAmount(model.Amount);
+                return RedirectToAction(nameof(Result), new { amount = result });
+            }
+            return View(model);
+        }
+
+        public IActionResult Result(double amount)
+        {
+            ViewData["amount"] = amount;
+            return View();
+        }
+
+        private double CalculateDeductableAmount(double amount)
+        {
+            const double taxRate = 20;
+            return amount * (taxRate / (100 - taxRate));
+        }
+    }
+}
---
+++ CharityCalculatorV2/CharityCalculatorV2/Models/DonationViewModel/DonationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharityCalculatorV2.Models.DonationViewModel
+{
+    public class DonationViewModel
+    {
+        [Required]
+        public double Amount { get; set; }
+    }
+}
